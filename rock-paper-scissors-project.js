94d63db4142ec5c3982297c7e008095b1c0d7a97
Yuliia Plaksytska <192624425+walkingmaze@users.noreply.github.com>
2025-06-27 08:19:24 +0200

    Write logic to get computer choice

--- rock-paper-scissors-project.js
+++ rock-paper-scissors-project.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+
+let min = 1;
+let max = 3;
+function getComputerChoice() { const getRandomNumber = Math.floor(Math.random()*(max-min+1))+min;
+if (getRandomNumber === 1) { 
+return 'paper';
+} else if (getRandomNumber === 2) {
+return 'rock';
+} else {
+return 'scissors';
+}
+}
