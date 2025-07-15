757b4a7ba97793db20cbcb31baaec0177bc107b1
Yuliia Plaksytska <192624425+walkingmaze@users.noreply.github.com>
2025-07-15 07:48:02 +0200

    add code for human logic

--- rock-paper-scissors-project.js
+++ rock-paper-scissors-project.js
@@ -10,15 +10,51 @@ return 'rock';
 return 'scissors';
 }
 }
-const choiceButton = document.querySelector("#choiceButton");
-const log = document.querySelector("#log");
 
-choiceButton.addEventListener("click", () => {
-  let choice = prompt("What your guess?");
-
-  if (choice === "paper" || choice === "rock" || choice === "scissors") {
-    log.innerText = "OK";
-  } else {
-    log.innerText = "Try again";
+function getHumanChoice() {
+  let choice = prompt("Enter your choice(rock, paper or scissors):").toLowerCase();
+  while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+    choice = prompt("Invalid choice. Please enter rock, paper, or scissors:")
   }
+  return choice;
+}
+
+const choiceButton = document.getElementById("choiceButton");
+choiceButton.addEventListener("click",function() {
+  const userChoice = getHumanChoice();
+  console.log("User choice:", userChoice);
 });
+
+let humanScore = 0;
+let computerScore = 0;
+let playRound;
+function playRound(getHumanChoice, getComputerChoice) {
+if (getHumanChoice === "rock" && getComputerChoice === "scissors") {
+  humanScore +=1;
+    return "You win. Rock breaks scissors";
+} else if (getHumanChoice === "paper" && getComputerChoice === "rock") {
+  humanScore +=1;
+    return "You win! Paper covers rock";
+} else if 
+  (getHumanChoice === "scissors" && getComputerChoice === "paper") {
+  humanScore +=1;
+    return "You win! Scissors cut paper";
+} else if (getHumanChoice === "scissors" && getComputerChoice === "rock") {
+  computerScore += 1;
+    return "You lose! Rock breaks scissors";
+} else if (getHumanChoice === "rock" && getComputerChoice === "paper") {
+  computerScore += 1;
+    return "You lose. Paper covers rock";
+} else if (getHumanChoice === "paper" && getComputerChoice === "scissors") {
+  computerScore += 1;
+    return "You lose! Scissors cut paper";
+} else {
+  humanScore += 1;
+  computerScore += 1;
+  return "The result is even. Try again."
+}
+  }
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
